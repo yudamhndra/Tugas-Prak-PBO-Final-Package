dfaf219217589e4ab3eccad35c1c9ff0283f365a
yudamhndra <110273182+yudamhndra@users.noreply.github.com>
2023-05-20 21:25:53 +0700

    Delete TugasFinalPackagePert11 directory

--- TugasFinalPackagePert11/src/Package1/Car.java
+++
@@ -1,7 +0,0 @@
-package Package1;
-
-Public class Car {
-    public void starterIntruction () {
-        System.out.println("Please bring the key closer");
-    }
-}
--- TugasFinalPackagePert11/src/Package2/Car.java
+++
@@ -1,7 +0,0 @@
-package Package2;
-
-public class Car {
-    public void starterIntruction () {
-        System.out.println("Please insert the key");
-    }
-}
